9fbf06c31bfd5c844040cbee943ac157ef02ec7d
Wei Wang <thomas.wei.wang@gmail.com>
2013-11-25 17:38:19 -0500

    started oral slides

---
+++ oral/auto/slides.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook "slides"
+ (lambda ()
+    (TeX-run-style-hooks
+     "footmisc"
+     "algorithmic"
+     "algorithm"
+     "amsfonts"
+     "epsfig"
+     "amsmath"
+     "ucs"
+     "subfig"
+     "lotdepth"
+     "lofdepth"
+     "xltxtra"
+     "fontspec"
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "xetex"
+     "mathserif"
+     "serif")))
+
